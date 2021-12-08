469b68c1cca06c392fe9e7c6107048df6f37dec2
nichollu <lubingqian617@126.com>
2021-12-07 19:47:43 -0800

    new model samples

--- tools/frontend/nasbench/get_model.py
+++ tools/frontend/nasbench/get_model.py
@@ -92,8 +92,16 @@ def get_model():
 		index = select_index[i]
 		arch = model_arch[index]
 		genotype = get_genotype_from_arch(arch)
+		print(arch, genotype)
+		from collections import namedtuple
+		Genotype = namedtuple('Genotype', 'normal normal_concat reduce reduce_concat')
+		DARTS_V2 = Genotype(normal=[('sep_conv_3x3', 0), ('sep_conv_3x3', 1), ('sep_conv_3x3', 0), ('sep_conv_3x3', 1), ('sep_conv_3x3', 1), ('skip_connect', 0), ('skip_connect', 0), ('dil_conv_3x3', 2)], normal_concat=[2, 3, 4, 5], reduce=[('max_pool_3x3', 0), ('max_pool_3x3', 1), ('skip_connect', 2), ('max_pool_3x3', 1), ('max_pool_3x3', 0), ('skip_connect', 2), ('skip_connect', 2), ('max_pool_3x3', 1)], reduce_concat=[2, 3, 4, 5])
+		genotype = DARTS_V2
 		print(i)
+		print(arch, genotype)
 		model = Network(36, CIFAR_CLASSES, 20, True, genotype)
+		print(model)
+		print(h)
 		all_model.append(model)
 				
 	return all_model

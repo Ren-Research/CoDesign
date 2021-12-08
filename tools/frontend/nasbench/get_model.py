5b4ccb8cd1e42e68649d67d2525537a428440eea
nichollu <lubingqian617@126.com>
2021-12-07 19:57:44 -0800

    Update get_model.py

--- tools/frontend/nasbench/get_model.py
+++ tools/frontend/nasbench/get_model.py
@@ -83,8 +83,9 @@ def get_model():
 	
 	CIFAR_CLASSES = 10
 	
-	model_arch = pickle.load(open("./nasbench/model_arch_depth.pickle", "rb"))
-	select_index = pickle.load(open("./nasbench/model_index_depth.pickle", "rb"))
+	model_arch = pickle.load(open("./nasbench/new/model_arch_depth.pickle", "rb"))
+	select_index = pickle.load(open("./nasbench/new/model_index_depth.pickle", "rb"))
+	depth = pickle.load(open("./nasbench/new/depth.pickle", "rb"))
 	print(len(select_index))
 	all_model = []
 	
@@ -92,16 +93,16 @@ def get_model():
 		index = select_index[i]
 		arch = model_arch[index]
 		genotype = get_genotype_from_arch(arch)
-		print(arch, genotype)
-		from collections import namedtuple
-		Genotype = namedtuple('Genotype', 'normal normal_concat reduce reduce_concat')
-		DARTS_V2 = Genotype(normal=[('sep_conv_3x3', 0), ('sep_conv_3x3', 1), ('sep_conv_3x3', 0), ('sep_conv_3x3', 1), ('sep_conv_3x3', 1), ('skip_connect', 0), ('skip_connect', 0), ('dil_conv_3x3', 2)], normal_concat=[2, 3, 4, 5], reduce=[('max_pool_3x3', 0), ('max_pool_3x3', 1), ('skip_connect', 2), ('max_pool_3x3', 1), ('max_pool_3x3', 0), ('skip_connect', 2), ('skip_connect', 2), ('max_pool_3x3', 1)], reduce_concat=[2, 3, 4, 5])
-		genotype = DARTS_V2
+#		print(arch, genotype)
+#		from collections import namedtuple
+#		Genotype = namedtuple('Genotype', 'normal normal_concat reduce reduce_concat')
+#		DARTS_V2 = Genotype(normal=[('sep_conv_3x3', 0), ('sep_conv_3x3', 1), ('sep_conv_3x3', 0), ('sep_conv_3x3', 1), ('sep_conv_3x3', 1), ('skip_connect', 0), ('skip_connect', 0), ('dil_conv_3x3', 2)], normal_concat=[2, 3, 4, 5], reduce=[('max_pool_3x3', 0), ('max_pool_3x3', 1), ('skip_connect', 2), ('max_pool_3x3', 1), ('max_pool_3x3', 0), ('skip_connect', 2), ('skip_connect', 2), ('max_pool_3x3', 1)], reduce_concat=[2, 3, 4, 5])
+#		genotype = DARTS_V2
 		print(i)
-		print(arch, genotype)
-		model = Network(36, CIFAR_CLASSES, 20, True, genotype)
-		print(model)
-		print(h)
+		#print(arch, genotype)
+		model = Network(36, CIFAR_CLASSES, depth, True, genotype)
+#		print(model)
+#		print(h)
 		all_model.append(model)
 				
 	return all_model
